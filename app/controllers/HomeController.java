ff69890a42d48e2d71da7ee6a8182909fdd5d4e2
Aidassj <aida.sajjadimilani@mail.concordia.ca>
2024-10-16 11:20:34 -0400

    GH-6: [feat] final version of my task in group part

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -2,12 +2,16 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.index; // Make sure to import the index view
-
+import views.html.index;
+/**
+ * This controller contains an action to handle HTTP requests to the application's home page.
+ *
+ * @author Deniz Dinchdonmez
+ */
 public class HomeController extends Controller {
 
   // This method renders the index page
   public Result index() {
-    return ok(index.render()); // No need to pass request header anymore
+    return ok(index.render());
   }
 }

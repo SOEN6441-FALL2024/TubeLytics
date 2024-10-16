e6846c3a8d7c0ddf462009e7c612c046864c3fa5
Aidassj <aida.sajjadimilani@mail.concordia.ca>
2024-10-16 11:46:55 -0400

    Updated code for web interface section (#15)
    * Updated HomeController and views
    
    * GH-6: [feat] my task in group part
    
    * GH-6: [feat] my task in group part
    
    * GH-6: [feat] final version of my task in group part

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,7 +1,8 @@
 package controllers;
 
-import play.mvc.*;
-
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 /**
  * This controller contains an action to handle HTTP requests to the application's home page.
  *
@@ -9,12 +10,8 @@ import play.mvc.*;
  */
 public class HomeController extends Controller {
 
-  /**
-   * An action that renders an HTML page with a welcome message. The configuration in the <code>
-   * routes</code> file means that this method will be called when the application receives a <code>
-   * GET</code> request with a path of <code>/</code>.
-   */
+  // This method renders the index page
   public Result index() {
-    return ok(views.html.index.render());
+    return ok(index.render());
   }
 }

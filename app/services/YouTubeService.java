f27a2246f768f6bb473071e908f74373e0f8b491
aynazjvn <aynazjavaniii@gmail.com>
2024-11-09 00:47:11 -0500

    GH-14: [feat] "Add new function to calculate word stats"

--- app/services/YouTubeService.java
+++ app/services/YouTubeService.java
@@ -1,52 +1,53 @@
 package services;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import models.Video;
 import com.typesafe.config.Config;
+import play.libs.ws.WSClient;
+
+import javax.inject.Inject;
 import java.util.List;
 import java.util.stream.Collectors;
-import javax.inject.Inject;
-import models.Video;
-import play.libs.ws.WSClient;
 
 public class YouTubeService {
 
-  private final String apiKey;
-  private final WSClient ws;
+    private final String apiKey;
+    private final WSClient ws;
 
-  @Inject
-  public YouTubeService(WSClient ws, Config config) {
-    this.ws = ws;
-    this.apiKey = "";
-  }
+    @Inject
+    public YouTubeService(WSClient ws, Config config) {
+        this.ws = ws;
+        this.apiKey = "";
+    }
 
-  public List<Video> searchVideos(String query) {
-    String youtubeUrl = "https://www.googleapis.com/youtube/v3/search";
-    String url =
-        String.format(
-            "%s?part=snippet&q=%s&type=video&maxResults=10&key=%s", youtubeUrl, query, apiKey);
+    public List<Video> searchVideos(String query) {
+        return this.searchVideos(query,10);
+    }
 
-    var futureResult =
-        ws.url(url)
-            .get()
-            .thenApply(
-                response -> {
-                  JsonNode items = response.asJson().get("items");
-                  return items.findValues("snippet").stream()
-                      .map(
-                          snippet -> {
-                            String title = snippet.get("title").asText();
-                            String description = snippet.get("description").asText();
-                            String channelId = snippet.get("channelId").asText();
-                            String channelTitle =
-                                snippet.get("channelTitle").asText(); // Fetch channel title
-                            String thumbnail =
-                                snippet.get("thumbnails").get("default").get("url").asText();
-                            return new Video(
-                                title, description, channelId, "", thumbnail, channelTitle);
-                          })
-                      .collect(Collectors.toList());
-                });
 
-    return futureResult.toCompletableFuture().join();
-  }
+    public List<Video> searchVideos(String query,int limit) {
+        String youtubeUrl = "https://www.googleapis.com/youtube/v3/search";
+        String url = String.format(
+                "%s?part=snippet&q=%s&type=video&maxResults=%d&key=%s", youtubeUrl, query, limit,apiKey);
+
+        var futureResult = ws.url(url)
+                .get()
+                .thenApply(
+                        response -> {
+                            JsonNode items = response.asJson().get("items");
+                            return items.findValues("snippet").stream()
+                                    .map(
+                                            snippet -> {
+                                                String title = snippet.get("title").asText();
+                                                String description = snippet.get("description").asText();
+                                                String channelId = snippet.get("channelId").asText();
+                                                String channelTitle = snippet.get("channelTitle").asText(); // Fetch channel title
+                                                String thumbnail = snippet.get("thumbnails").get("default").get("url").asText();
+                                                return new Video(title, description, channelId, "", thumbnail, channelTitle);
+                                            })
+                                    .collect(Collectors.toList());
+                        });
+
+        return futureResult.toCompletableFuture().join();
+    }
 }

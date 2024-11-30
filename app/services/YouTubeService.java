c02b11961f5f08972786375b88624f6bbb3d0b4f
Aidassj <aida.sajjadimilani@mail.concordia.ca>
2024-11-30 14:32:54 -0500

    remove api

--- app/services/YouTubeService.java
+++ app/services/YouTubeService.java
@@ -18,7 +18,7 @@ public class YouTubeService {
     @Inject
     public YouTubeService(WSClient ws, Config config) {
         this.ws = ws;
-        this.apiKey = "AIzaSyBPhuE2lB0jBpSoz5B0CEzGbIOjlC1rLLk";
+        this.apiKey = "";
     }
 
     public CompletionStage<List<Video>> searchVideos(String query, int limit) {
